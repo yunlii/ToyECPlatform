20c15fff701d36c812f878a1fec7212b7d66f1ec
linmoubin <aa1393923019@qq.com>
2025-06-12 21:01:13 +0800

    working day on June 12th.

--- pages/index/recommend/recommend.js
+++ pages/index/recommend/recommend.js
@@ -7,7 +7,6 @@ Page({
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,5 +61,46 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+
+  data: {
+    toView: 'green'
+  },
+
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
   }
 })
